6e3fd7da5d70062b3ca0b9fb1277380dc684d4f9
xzlwhy <63728855+xzlwhy@users.noreply.github.com>
2021-07-27 11:54:39 +0800

    Update BaseTable.vue

--- src/views/baseTable/BaseTable.vue
+++ src/views/baseTable/BaseTable.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+		基础
     <SimpleTable :tableConfig="tableConfig" :tableData="tableData" />
     <SimpleTable :tableConfig="tableConfig2" :tableData="tableData2" />
     <Pagination
